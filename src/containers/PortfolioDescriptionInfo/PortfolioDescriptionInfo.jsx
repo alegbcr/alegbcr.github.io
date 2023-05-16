6326c6f4805ea2de49bbb32698077b177aa831bd
Alexander González B <alexgbcr@proton.me>
2023-05-16 11:39:50 -0600

    [UPDATE] final de la version 2

--- src/containers/PortfolioDescriptionInfo/PortfolioDescriptionInfo.jsx
+++ src/containers/PortfolioDescriptionInfo/PortfolioDescriptionInfo.jsx
@@ -1,7 +1,76 @@
 import styled from "styled-components";
 import { SectionBase } from "../../components/SectionBase";
+import { ContainerBase } from "../../components/ContainerBase";
 
-export const Section = styled(SectionBase)``;
+export const Section = styled(SectionBase)`
+  padding: 0;
+  height: 100%;
+`;
+
+export const Picture = styled.picture`
+  display: flex;
+  align-items: end;
+  justify-content: center;
+`;
+
+export const Image = styled.img`
+  width: 300px;
+`;
+
+export const Container = styled(ContainerBase)`
+  &.info_main__container {
+    justify-self: center;
+    grid-column: 1 / -1;
+    grid-row: 3 / 5;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &.info_main__container,
+  &.works_main__container {
+    width: 85%;
+  }
+
+  /* Company works */
+  &.works_main__container {
+    justify-self: center;
+    grid-column: 1 / -1;
+    grid-row: 5 / -1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    &.info_main__container {
+      justify-self: center;
+      grid-column: 2 / 6;
+      grid-row: 4 / 8;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    /* Company works */
+    &.works_main__container {
+      grid-column: 6 / -1;
+      grid-row: 4 / -1;
+    }
+  }
+`;
+
+export const List = styled.ul``;
+
+export const ItemList = styled.li`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+`;
 
 export const Button = styled.button`
   border: none;
--- src/containers/PortfolioDescriptionInfo/index.jsx
+++ src/containers/PortfolioDescriptionInfo/index.jsx
@@ -1,120 +1,94 @@
 import React from "react";
 
-import { Section, Button } from "./PortfolioDescriptionInfo";
+import {
+  Section,
+  Button,
+  Picture,
+  Image,
+  Container,
+  List,
+  ItemList,
+} from "./PortfolioDescriptionInfo";
 
 const PortfolioDescriptionInfo = (props) => {
   return (
-    <Section className="portfolio-description__section">
-      <picture
-        style={{
-          gridColumn: "1 / -1",
-          gridRow: "1 / 6",
-          alignSelf: "end",
-          justifySelf: "center",
-          display: "flex",
-        }}
-      >
-        <img
-          src={props.companyLogo}
-          style={{
-            width: "350px",
-            display: "flex",
-          }}
-        />
-      </picture>
-      <div
-        style={{
-          gridColumn: "1 / -1",
-          gridRow: "6 / -1",
-          width: "85%",
-          justifySelf: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            margin: "0 auto",
-            justifyContent: "space-between",
-            marginTop: ".8rem",
-          }}
-        >
-          <h2>Categoría</h2>
-          <p>{props.profesion}</p>
-        </div>
-        <div
-          style={{
-            gridColumn: "1 / -1",
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: ".8rem",
-          }}
-        >
-          <h2>Año</h2>
-          <p>{props.age}</p>
-        </div>
-        <div
-          style={{
-            gridColumn: "1 / -1",
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: ".8rem",
-          }}
-        >
-          <h2>Cliente</h2>
-          <p>{props.company}</p>
-        </div>
-        <p
-          style={{
-            gridColumn: "1 / -1",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            marginTop: ".8rem",
-          }}
-        >
-          {props.description.map((paragraph) => {
-            return <p key={paragraph.paragraph}>{paragraph.paragraph}</p>;
-          })}
-        </p>
-        {props.links ? (
+    <>
+      <Section className="info__section">
+        <Container className="info_main__container">
+          <Picture>
+            <Image src={props.companyLogo} />
+          </Picture>
+          {/* Company Descripton */}
+          <Container className="info__container">
+            <List
+              style={{
+                display: "flex",
+                margin: "1rem auto",
+                flexDirection: "column",
+              }}
+            >
+              <ItemList>
+                <h3>Categoría</h3>
+                <p>{props.profesion}</p>
+              </ItemList>
+              <ItemList>
+                <h3>Año</h3>
+                <p>{props.age}</p>
+              </ItemList>
+              <ItemList>
+                <h3>Cliente</h3>
+                <p>{props.company}</p>
+              </ItemList>
+            </List>
+            {props.description.map((paragraph) => {
+              return <p key={paragraph.paragraph}>{paragraph.paragraph}</p>;
+            })}
+          </Container>
+        </Container>
+
+        {/* Company Works */}
+
+        <Container className="works_main__container">
+          {props.links ? (
+            <div>
+              <h3>Landing Page</h3>
+              <picture style={{ display: "flex", maxWidth: "100%" }}>
+                <img
+                  src={props.image}
+                  style={{ width: "100%", display: "flex", alignItems: "end" }}
+                />
+              </picture>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: ".5rem 0",
+                }}
+              >
+                {props.links.map((link) => {
+                  return (
+                    <Button key={link.name} style={{ margin: "0 0.5rem" }}>
+                      <a href={`${link.link}`}>{link.name}</a>
+                    </Button>
+                  );
+                })}
+              </div>
+            </div>
+          ) : null}
+
           <div>
-            <h3>Landing Page</h3>
+            <h3>Diseño Gráfico</h3>
             <picture style={{ display: "flex", maxWidth: "100%" }}>
               <img
-                src={props.image}
+                src={props.imageRRSS}
                 style={{ width: "100%", display: "flex", alignItems: "end" }}
               />
             </picture>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: ".5rem 0",
-              }}
-            >
-              {props.links.map((link) => {
-                return (
-                  <Button key={link.name} style={{ margin: "0 0.5rem" }}>
-                    <a href={`${link.link}`}>{link.name}</a>
-                  </Button>
-                );
-              })}
-            </div>
           </div>
-        ) : null}
-
-        <div>
-          <h3>Diseño Gráfico</h3>
-          <picture style={{ display: "flex", maxWidth: "100%" }}>
-            <img
-              src={props.imageRRSS}
-              style={{ width: "100%", display: "flex", alignItems: "end" }}
-            />
-          </picture>
-        </div>
-      </div>
-    </Section>
+        </Container>
+      </Section>
+    </>
   );
 };
 
